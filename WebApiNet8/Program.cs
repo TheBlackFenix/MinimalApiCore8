f65adaff101dd292165b84653d08c04c2aa01f6b
Daniel Hernando Miranda Tellez <danielh1991@hotmail.com>
2024-07-20 23:09:35 -0500

    Creacion de Endpoint Generos y configuración de politicas de CORS.

--- WebApiNet8/Program.cs
+++ WebApiNet8/Program.cs
@@ -1,6 +1,56 @@
+using Microsoft.AspNetCore.Cors;
+using WebApiNet8.Entidades;
+
 var builder = WebApplication.CreateBuilder(args);
+string AllowedHosts = builder.Configuration.GetValue<string>("AllowedHosts")!;
+#region Servicios
+// Aquí van todos los servicios que se van a utilizar
+
+//Configuracion de CORS
+builder.Services.AddCors(options =>
+{
+    {
+        options.AddDefaultPolicy(builder =>
+        {
+            builder.WithOrigins(AllowedHosts)
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+        options.AddPolicy("AllowAll", builder =>
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+    }
+});
+
+
+#endregion
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+#region Middlewares 
+// Aquí van todos los middlewares que se van a utilizar
+
+// Middleware para aplicar politica de CORS
+app.UseCors();
+
+// Middleware Endpoints
+app.MapGet("/Generos", [EnableCors(policyName:"AllowAll")]() =>
+{
+    var generos = new List<Genero>
+    {
+        new Genero { IdGenero = 1, NombreGenero = "Comedia" },
+        new Genero { IdGenero = 2, NombreGenero = "Drama" },
+        new Genero { IdGenero = 3, NombreGenero = "Acción" },
+        new Genero { IdGenero = 4, NombreGenero = "Romance" },
+        new Genero { IdGenero = 5, NombreGenero = "Ciencia Ficción" }
+    };
+    return generos;
+});
+
+
+#endregion
 
 app.Run();
